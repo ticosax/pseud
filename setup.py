2122efe330da8634d33f19b191847ea9c0d7113f
Nicolas Delaby <nicolas.delaby@ezeep.com>
2013-11-14 16:07:12 +0100

    initial commit for pyzmq-rpc

---
+++ setup.py
@@ -0,0 +1,42 @@
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+version = '0.0.1dev'
+
+
+setup(name='pyzmq-rpc',
+      version=version,
+      description='RPC Api on top of pzmq',
+      author='Nicolas Delaby',
+      author_email='nicolas.delaby@ezeep.com',
+      url='',
+      package_dir={'': 'src'},
+      packages=['pyzmq_rpc'],
+      zip_safe=True,
+      install_requires=[
+          'pyzmq',
+          'msgpack-python',
+          'tornado',
+          'futures',
+          ],
+      tests_require=[
+          'pytest',
+          ],
+      cmdclass={'test': PyTest},
+      )
