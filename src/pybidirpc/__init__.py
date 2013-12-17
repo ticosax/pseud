b03d787175cc4ed8820f4687847560ca391e062e
Nicolas Delaby <nicolas.delaby@ezeep.com>
2013-12-17 18:21:58 +0100

    Provide a flag to choose one backend if both are available

--- src/pybidirpc/__init__.py
+++ src/pybidirpc/__init__.py
@@ -1,4 +1,7 @@
+import os
 try:
     from ._tornado import Client, Server
+    if os.getenv('NO_TORNADO'):
+        raise ImportError
 except ImportError:
     from ._gevent import Client, Server  # NOQA
