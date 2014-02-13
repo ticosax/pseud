5b9d82529669bc0e10c142a3329c708aa42b04ea
Nicolas Delaby <nicolas.delaby@ezeep.com>
2014-02-13 17:43:26 +0100

    develop now 0.0.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read_that_file(path):
 long_description = '\n'.join((read_that_file('README.rst'),
                               read_that_file('LICENSE.txt')))
 
-version = '0.0.2'
+version = '0.0.3dev'
 
 
 setup(name='pseud',
