a465f7874f2ad93765116e7f1bfb31db52b3c03b
Nicolas Delaby <nicolas.delaby@ezeep.com>
2014-06-06 21:39:08 +0200

    typos

--- tests/tornado_based/test_auth.py
+++ tests/tornado_based/test_auth.py
@@ -298,7 +298,7 @@ class CurveTestCase(tornado.testing.AsyncTestCase):
         client.stop()
 
     @pytest.mark.skipif(zmq.zmq_version_info() < (4, 1, 0),
-                        reason='Needs zeromq build with libzmq >= 4.1.0')
+                        reason='Needs pyzmq build with libzmq >= 4.1.0')
     @tornado.testing.gen_test()
     def test_client_can_reconnect(self):
         from pseud import Client, Server
