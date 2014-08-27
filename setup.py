d6be4e61d22e2487a2e43f63e51c93648e0e327a
Nicolas Delaby <ticosax@free.fr>
2014-08-27 20:04:34 +0200

    Back to dev

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read_that_file(path):
 long_description = '\n'.join((read_that_file('README.rst'),
                               read_that_file('LICENSE.txt')))
 
-version = '0.0.5'
+version = '0.0.6dev'
 
 
 setup(name='pseud',
